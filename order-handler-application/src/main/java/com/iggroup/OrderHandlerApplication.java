85f0484e853e0375f95854c298b35d8775180adc
Divit Bui <divitngoc@gmail.com>
2021-05-22 16:30:05 +0100

    Clean up on OrderHandlerApplicationClass
    Remove some unnecessary code from playing around

--- order-handler-application/src/main/java/com/iggroup/OrderHandlerApplication.java
+++ order-handler-application/src/main/java/com/iggroup/OrderHandlerApplication.java
@@ -1,36 +1,21 @@
 package com.iggroup;
 
 import com.iggroup.handler.DefaultOrderHandler;
-import com.iggroup.model.OrderBook;
 import com.iggroup.model.Side;
 import com.iggroup.producer.OrderProducer;
 import com.iggroup.provider.OrderBookProvider;
 import com.iggroup.service.TradeService;
-import com.iggroup.util.PrinterUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class OrderHandlerApplication {
 
-    /*
-     * 
-     * ================================== SYMBOL [IGG] ==================================
-     * | Order Count| Ask Quantity| Ask Price| Level| Bid price| Bid Quantity| Order Count|
-     * |==================================================================================|
-     * | 1          | 7           | 25       | 1    | 13       | 3           | 2          |
-     * | 3          | 23          | 28       | 2    | 12       | 5           | 1          |
-     * | 1          | 5           | 30       | 3    | -        | -           | -          |
-     * 
-     */
     public static void main(String[] args) throws InterruptedException {
         DefaultOrderHandler orderHandler = new DefaultOrderHandler(OrderBookProvider.getInstance(),
                                                                    TradeService.getInstance());
         startProducer(orderHandler);
-        // Thread.sleep(1000);
         startProducer(orderHandler);
-        OrderBook iggOrderBook = OrderBookProvider.getInstance()
-                                                  .getOrderBookBySymbol("IGG");
 
         while (true) {
             log.info("Average SELL Price: {} and Average BUY Price: {}", orderHandler.getPrice("IGG", 3, Side.SELL),
